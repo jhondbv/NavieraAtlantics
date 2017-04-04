a7bd94caa6117cc446c53f08c89098ee165b9ac9
Penagos <Penagos@PENAGOS>
2017-04-04 01:12:38 -0500

    Se modifica la logica dePuertos

--- src/PresentationLayer/NavieraMain.java
+++ src/PresentationLayer/NavieraMain.java
@@ -97,6 +97,11 @@ public class NavieraMain extends javax.swing.JFrame {
         btnViajes4.setText("Viajes");
 
         btnPuertos.setText("Puertos");
+        btnPuertos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPuertosActionPerformed(evt);
+            }
+        });
 
         jLabel1.setText("NAVIERA ");
 
@@ -180,6 +185,10 @@ public class NavieraMain extends javax.swing.JFrame {
          // this.setVisible(false);
         new EsposasView().setVisible(true);
     }//GEN-LAST:event_btnEsposasActionPerformed
+
+    private void btnPuertosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPuertosActionPerformed
+        new PuertosView().setVisible(true);
+    }//GEN-LAST:event_btnPuertosActionPerformed
 
     /**
      * @param args the command line arguments
